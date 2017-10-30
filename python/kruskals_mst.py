e579e1cfa6c915a640eb1581f84327d8d66186fe
Lidiya Murakhovs'ka <lidiya.murakhovskaya@gmail.com>
2017-10-29 22:58:39 -0400

    kruskals mst

---
+++ python/kruskals_mst.py
@@ -0,0 +1,74 @@
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
+
+    def add_edge(self, u, v, w):
+        self.graph.append([u, v, w])
+
+    # Find set of an element i (uses path compression technique)
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+
+    # Union of two sets of x and y (uses union by rank)
+    def union(self, parent, rank, x, y):
+        xroot = self.find(parent, x)
+        yroot = self.find(parent, y)
+
+        # Attach smaller rank tree under root of high rank tree (Union by Rank)
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+        # If ranks are same, then make one as root and increment its rank by 1
+        else:
+            parent[yroot] = xroot
+            rank[xroot] += 1
+
+    def KruskalMST(self):
+        result = []
+        i = 0
+        e = 0
+
+        # Step 1:  Sort all the edges by weight
+        self.graph = sorted(self.graph, key=lambda item: item[2])
+
+        parent = []
+        rank = []
+
+        # Create V subsets with single elements
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+
+        while e < self.V - 1:
+
+            # Step 2: Pick the smallest edge, increment
+            u, v, w = self.graph[i]
+            i = i + 1
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+
+            if x != y:
+                e = e + 1
+                result.append([u, v, w])
+                self.union(parent, rank, x, y)
+
+        print "Edges in the constructed MST:"
+        for u, v, weight in result:
+            print ("%d -- %d == %d" % (u, v, weight))
+
+
+g = Graph(4)
+g.add_edge(0, 1, 10)
+g.add_edge(0, 2, 6)
+g.add_edge(0, 3, 5)
+g.add_edge(1, 3, 15)
+g.add_edge(2, 3, 4)
+
+g.KruskalMST()
