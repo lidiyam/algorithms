61928335cbad396fbe5e29b26ab0c858e96445a0
Lidiya Murakhovs'ka <lidiya.murakhovskaya@gmail.com>
2018-03-04 20:26:32 -0500

    sequence alignment

--- python/sequence_alignment.py
+++ python/sequence_alignment.py
@@ -1,27 +1,36 @@
-def dp_alignmt(A, X, Y, gap, match):
+def match(xi, yj, match_v):
+		if xi != yj: return match_v
+		return 0
+
+def dp_alignmt(A, X, Y, gap, match_v):
 	m = len(X)
 	n = len(Y)
 
-	for i in range(m):
+	if m == 0 and n == 0: return []
+
+	for i in range(m+1):
 		A[i][0] = i*gap
 
-	for j in range(n):
+	for j in range(n+1):
 		A[0][j] = j*gap
 
-	for i in range(m):
-		for j in range(n):
-			A[i][j] = min(A[i-1][j-1] + match(X[i],Y[j]), A[i-1][j] + gap, A[i][j-1] + gap)
+	for i in range(1,m+1):
+		for j in range(1,n+1):
+			A[i][j] = min(A[i-1][j-1] + match(X[i-1],Y[j-1], match_v), A[i-1][j] + gap, A[i][j-1] + gap)
 
-	return A[m-1][n-1]
+	return A[m][n]
 
 
-def dp_alignmt_reconst(A, X, Y, gap, match):
-	i = len(X) - 1
-	j = len(Y) - 1
+def dp_alignmt_reconst(A, X, Y, gap, match_v):
+	i = len(X)
+	j = len(Y)
 	B = []
 
-	while i >= 0 and j >= 0:
-		if A[i][j] == A[i-1][j-1] + match(X[i],Y[j]):
+	X = ' ' + X
+	Y = ' ' + Y
+
+	while i > 0 and j > 0:
+		if A[i][j] == A[i-1][j-1] + match(X[i],Y[j],match_v):
 			B.insert(0, (X[i], Y[j]))
 			i -= 1
 			j -= 1
@@ -32,6 +41,14 @@ def dp_alignmt_reconst(A, X, Y, gap, match):
 			B.insert(0, ('-', Y[j]))
 			j -= 1
 
+	while i > 0:
+		B.insert(0, (X[i], '-'))
+		i -= 1
+
+	while j > 0:
+		B.insert(0, ('-', Y[j]))
+		j -= 1
+
 	return B
 
 
@@ -41,17 +58,9 @@ if __name__ == '__main__':
 	gap = int(raw_input())
 	match_v = int(raw_input())
 
-	#X = 'AGGAATT'
-	#Y = 'AGGCTT'
-	#gap = 1
-
-	def match(xi, yj):
-		if xi != yj: return match_v
-		return 0
-
-	A = [[0]*len(Y) for _ in range(len(X))]
-	min_c = dp_alignmt(A, X, Y, gap, match)
-	B = dp_alignmt_reconst(A, X, Y, gap, match)
+	A = [[0]*(len(Y)+1) for _ in range(len(X)+1)]
+	min_c = dp_alignmt(A, X, Y, gap, match_v)
+	B = dp_alignmt_reconst(A, X, Y, gap, match_v)
 
 	a, m, b = '', '', ''
 	for i, j in B:
